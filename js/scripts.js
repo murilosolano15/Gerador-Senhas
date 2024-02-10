8b8dbff5e698d967a1d8f2290433ee1bec67c9cf
Murilo Solano Oliva <murilo.solano@hotmail.com>
2024-02-10 20:12:31 -0300

    Projeto Gerador de senhas em HTML, CSS e JS

---
+++ js/scripts.js
@@ -0,0 +1,93 @@
+// Seleção de elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatePasswordElement = document.querySelector("#generated-password");
+
+// Novas funcionalidades
+const openCloseGeneratorButton = document.querySelector("#open-generate-password");
+const generatePasswordContainer = document.querySelector("#generate-options");
+const lengthInput = document.querySelector("#length");
+const lettersInput = document.querySelector("#letters");
+const numbersInput = document.querySelector("#numbers");
+const symbolsInput = document.querySelector("#symbols");
+const copyPasswordButton = document.querySelector("#copy-password");
+
+// Funções
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+const getNumbers = () => {
+    return Math.floor(Math.random() * 10).toString();
+}
+
+const getSymbols = () => {
+    const symbols = "()[]{}=<>/,.!@#$&@*+-?|";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumbers, getSymbols) => {
+    let password = "";
+
+    // Segunda versão
+    const passwordLength = +lengthInput.value;
+
+    const generators = [];
+
+    if (lettersInput.checked) {
+        generators.push(getLetterLowerCase, getLetterUpperCase)
+    }
+
+    if (numbersInput.checked) {
+        generators.push(getNumbers)
+    }
+
+    if (symbolsInput.checked) {
+        generators.push(getSymbols)
+    }
+
+    if (lettersInput.length === 0) {
+        return;
+    }
+
+    console.log(generators.length)
+
+    for (i = 0; i < passwordLength; i = i + generators.length) {
+        generators.forEach(() => {
+            const randomValue = generators[Math.floor(Math.random() * generators.length)]();
+
+            password += randomValue;
+        });
+    }
+
+    password = password.slice(0, passwordLength);
+
+    generatePasswordElement.style.display = "block"
+    generatePasswordElement.querySelector("h4").innerText = password;
+}
+
+// Eventos
+generatePasswordButton.addEventListener("click", () => {
+    generatePassword(getLetterLowerCase, getLetterUpperCase, getNumbers, getSymbols);
+});
+
+openCloseGeneratorButton.addEventListener("click", () => {
+    generatePasswordContainer.classList.toggle("hide");
+})
+
+copyPasswordButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const password = generatePasswordElement.querySelector("h4").innerText;
+
+    navigator.clipboard.writeText(password).then(() =>{
+        copyPasswordButton.innerText = "Senha copiada com sucesso"
+
+    setTimeout(() => {
+        copyPasswordButton.innerText = "Copiar";
+    }, 1000)
+    });
+});
